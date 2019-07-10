3ad33658b9449f65c07c972f5bef6bcc3669ad6d
Roger <idevelopedreams@gmail.com>
2019-07-10 11:05:23 -0400

    defining controllers for routes

---
+++ controllers/todo.js
@@ -0,0 +1,28 @@
+exports.allTodos = (req, res) => {
+    let sql = 'SELECT * FROM task';
+    req.context.db.query(sql, function (err, results) {
+        if (err) throw err;
+        // rendering tasks view and passing taskToDo data
+        res.render('tasks', {taskToDo: results});
+    });
+}
+
+exports.addTask = (req, res) => {
+    let task = req.body
+    let sql = 'INSERT INTO task SET ?';
+    req.context.db.query(sql, task, function (err, results) {
+        if (err) throw err;
+        // rendering tasks view and passing taskToDo data
+        console.log(results)
+        res.redirect('/')
+    });
+}
+
+exports.removeTask = (req, res) => {
+    let sql = 'DELETE  FROM task WHERE ID=' + req.params.id;
+    req.context.db.query(sql,(err, result) =>{
+        if(err) throw err;
+        console.log(result);
+        res.json(result)
+    })
+}
